5b211d98326356e1e2a32c453796847f4b5662f6
Mark Tobler <marktobler@gmail.com>
2021-03-06 08:33:07 -0800

    es6 workarounds

--- pdc.js
+++ pdc.js
@@ -1,13 +1,21 @@
-// get location
-var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
-// replace current_page with 'header'
-var page = newURL.substring(newURL.lastIndexOf('/')+1, newURL.indexOf('.html'));
-console.log(page);
-newURL = newURL.replace(page, 'header');
-console.log(newURL);
+function getURL(neededPage) {
+   // get location
+   var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+   // replace current_page with 'header'
+   var page = newURL.substring(newURL.lastIndexOf('/') + 1, newURL.indexOf('.html'));
+   console.log(page);
+   newURL = newURL.replace(page, neededPage);
+   console.log(newURL);
+   return newURL;
+}
 
+// To support older browsers, falling back to pure javascript
 function setHeader(hFile) {
    document.querySelector("header").innerHTML = hFile;
+}
+
+function setFooter(fFile) {
+   document.querySelector("header").innerHTML = fFile;
 }
 
 document.addEventListener(
@@ -15,12 +23,20 @@ document.addEventListener(
    () => {
 
       var req = new XMLHttpRequest();
-      req.onload = function(){
+      req.onload = function() {
          setHeader(this.responseText);
       };
-      req.open('GET', newURL);
+      req.open('GET', getURL('header'));
       req.send();
 
+      var fReq = new XMLHttpRequest();
+      fReq.onload = function () {
+         setFooter(this.responseText);
+      };
+      fReq.open('GET', getURL('footer'));
+      fReq.send();
+
+      /// can't use es6+ here.
       // fetch("./header.html")
       //    .then(response => {
       //       return response.text()
@@ -29,13 +45,13 @@ document.addEventListener(
       //       document.querySelector("header").innerHTML = data;
       //    });
 
-      fetch("./footer.html")
-         .then(response => {
-            return response.text()
-         })
-         .then(data => {
-            document.querySelector("footer").innerHTML = data;
-         });
+      // fetch("./footer.html")
+      //    .then(response => {
+      //       return response.text()
+      //    })
+      //    .then(data => {
+      //       document.querySelector("footer").innerHTML = data;
+      //    });
    }
 );
 
